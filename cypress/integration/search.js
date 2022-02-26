ccc82ba0a5680940e75e41a438de663ba76dc0ba
luisafcg <celeita.luisafernanda@gmail.com>
2022-02-25 19:59:47 -0500

    Se crea un command

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -3,26 +3,16 @@ describe ('Search elements', function(){
         cy .visit ('/');
     })
     it ('Search elements with multiples results',() =>{
-       cy.fixture('index').then((index) =>{
-           cy.get(index.elementSearch).type('dress');
-           cy.get(index.btnSearch).click();
-       })
-
-       cy.fixture('searchResults').then((search =>{
+           cy.search('dress')
+           cy.fixture('searchResults').then((search =>{
            cy.get(search.lighter).should('contain', 'dress');
        }))
     })
 
     it ('Search elements with not results', () =>{
-        cy.fixture('index').then((index) =>{
-          cy.get(index.elementSearch).type('Un vestido rojo');
-          cy.get(index.btnSearch).click();  
-        })
-
-        cy.fixture('searchResults').then((search =>{
+          cy.search('Vestido rojo')
+          cy.fixture('searchResults').then((search =>{
           cy.get(search.mensajeAlerta).should('contain', 'No results were found for your search');
         }))
-
-
     })
 })
